cdb3600913c6a928d001f558c28e8051caead9b9
jack_sparrow <ganeshm9042@gmail.com>
2025-03-21 10:41:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// const text=document.querySelector(".second-text");
+// const textload=()=>{
+//     setTimeout(()=>{
+//        text.textContent="Web developer";
+//     }, 0);
+//     setTimeout(()=>{
+//         text.textContent="Video Editer";
+//      }, 4000);
+//      setTimeout(()=>{
+//         text.textContent="Frentend Developer";
+//      }, 8000);
+// }
